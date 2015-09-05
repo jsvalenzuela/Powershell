8b4b36fbdecb96a73c89cad2fbdda78fe1d8632a
escurragonzalez <escurra.gonzalez@gmail.com>
2015-09-05 14:15:52 -0300

    Validaciones Ejercicio 2
    Modificación en validaciones, comentarios

--- Ejercicio_2/ejercicio2.ps1
+++ Ejercicio_2/ejercicio2.ps1
@@ -7,9 +7,12 @@
 # una cadena determinada. Debe recibir por parámetro la cadena a buscar, el directorio      #
 # de origen  y el de destino.                                                               #
 # directorio. Ambos directorios deben ser pasados por parámetro                             #
-# ALUMNOS :                                                                                 #
-#           - 
-#           -Gustavo Gonzalez                                                               #
+# ALUMNOS :                                                                                 #                                                                              
+#           -Bogado, Sebastian                                                              #
+#           -Camacho, Manfred                                                               #
+#           -Cruz, Juan                                                                     #
+#           -Gustavo, Gonzalez                                                              #
+#           -Valenzuela, Santiago                                                           #
 # EjemploEj.:                                                                               #
 # PS D:\tp> .\Ejercicio2.ps1 -pathOrigen 'E:\DC COMICS' -pathDestino E:\tp  -cadena Prueba  #
 #############################################################################################
@@ -23,9 +26,12 @@ El script Copia a un directorio todos los archivos de texto que contengan una ca
 Debe recibir por parámetro la cadena a buscar, el directorio de origen y el de destino.
 Al finalizar la copia, se debe crear un archivo de log en donde se indique el directorio de origen, 
 el tamaño y la fecha de modificación de cada uno de los archivos copiados.
-.PARAMETER computername
 
-.PARAMETER filePath
+.PARAMETER pathOrigen
+Ruta del directorio que se desea buscar archivos que contengan la cadena determinada
+
+.PARAMETER pathDestino
+Ruta donde se copiaran los arhcivos
 
 .EXAMPLE
 
@@ -40,11 +46,14 @@ Param
 [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $pathDestino,
 [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $cadena
 )
+
+$cantPar = ($psboundparameters.Count + $args.Count)
 if( $cantPar -ne 3 )
 {
   Write-Output "La cantidad parametros fue distinta a 3!"
   exit
 }
+#Validaciones de Path 
 $pathValidDest = Test-Path $pathDestino;
 $pathValidOrigen = Test-Path $pathOrigen;
 if($pathValidDest -eq $false)
@@ -63,19 +72,32 @@ if($pathDestino -eq $pathOrigen)
     echo "Path de origen y de destino no peden ser el mismo";
     exit;
 }
+
 if($pathValidOrigen -eq $true -and $pathValidDest -eq $true )
-{try{
+{
+    try{
+    #Obtengo lista de archivos 
     $archivos = ls -Path $pathOrigen -Recurse -Include "*.txt" | Where-Object {$_.Name -match "$cadena"};           
-}
-Catch [UnauthorizedAccessException],[DirUnauthorizedAccessError]{
+    }
+    Catch [UnauthorizedAccessException],[DirUnauthorizedAccessError]{
     Write-Output "No tiene permisos"
-}
-Catch{
+    }
+    Catch{
     Write-Output "Error al copiar archivos"
+    }
 }
 foreach($item in $archivos)
-{   
-    cp $archivos $pathDestino;
+{
+    try{   
+        cp $archivos $pathDestino;
+    }
+    Catch [UnauthorizedAccessException],[DirUnauthorizedAccessError]{
+    Write-Output "No tiene permisos"
+    }
+    Catch
+    {
+        Write-Output "Error al copiar arhcivos"
+    }
+    #Genero Log
     $archivos| Format-List -Property Directory,Length,LastWriteTime > .\Log.txt
-}
 }
