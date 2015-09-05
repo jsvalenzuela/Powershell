3a84987e8671e3e9fef5e91d23cbf58c9a6fde4e
Manfred Camacho <manfredcb+gh@gmail.com>
2015-09-05 02:21:44 -0300

    Corregido ejercicio 5

--- Ejercicio_5/Ejercicio5.ps1
+++ Ejercicio_5/Ejercicio5.ps1
@@ -4,38 +4,37 @@
 # TIPO DE PROGRAMA: .ps1                                                                    #
 # ARCHIVOS DE SALIDA : El backup tiene que ser la fecha del backup YYYYMMDD.zip             #
 # COMENTARIOS:                                                                              # 
-# ALUMNOS :                                                                                 #
-#           -Gustavo Gonzalez                                                               #
+# ALUMNOS :                                                                                 #                                                                              #
+#           -Bogado, Sebastian                                                              #
+#           -Camacho, Manfred                                                               #
+#           -Cruz, Juan                                                                     #
+#           -Gustavo, Gonzalez                                                               #
+#           -Valenzuela, Santiago                                                           #
 # Ejemplo Ej.:                                                                              #
-# C:\PS> .\ejercicio1.ps1 -pathOrigen d:\Archivos -pathDestino d:\Backup                    #
+# C:\PS> .\ejercicio5.ps1 -pathOrigen D:\Archivos -pathDestino D:\Backup                    #
 #############################################################################################
 
 <#
-
 .SYNOPSIS
-TP2 - PowerShell
-
+TP5 - PowerShell
 
 .DESCRIPTION
 Realiza un backup de un directorio especificado por parámetro
-Se recibe por parámetro el directorio que contiene los archivos subidos por los usuarios
-Se recibe por parámetro el directorio en donde se guardará el backup.
-El nombre del backup tiene el siguiente formato YYYYMMDD.zip
+El archivo .zip del backup se guarda con el siguiente formato YYYYMMDD.zip
 
-.PARAMETER computername
+.PARAMETER pathOrigen
+Ruta del directorio que se desea hacer el backup.
 
-.PARAMETER filePath
+.PARAMETER pathDestino
+Ruta donde se guardará el backup (.zip).
 
-.EXAMPLE
-    C:\PS> .\ejercicio1.ps1 -pathOrigen d:\Archivos -pathDestino d:\Backup
+.EXAMPLED
+    C:\PS> .\ejercicio5.ps1 -pathOrigen D:\Archivos -pathDestino D:\Backup
 
 .NOTES
     Si hay más de 3 archivos .zip en el directorio del backup se borra el más antiguo
-
-.LINK
-N/A
-
 #>
+
 param(
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $pathOrigen,
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $pathDestino
@@ -54,17 +53,7 @@ try
     if(test-path -Path $pathOrigen)
     {
         if(test-path -Path $pathDestino)
-        {
-            #Archivos .zip ordenados ascendentemente por fecha de creacion (primero el mas antiguo), se guardan en un array
-            $Archivos = ls -LiteralPath $pathOrigen -File *.zip| Sort-Object -Property CreationTime
-            #Si hay mas de 3 archivos .zip
-            if ($Archivos.Length -gt 3)
-            {
-                #con -First n indico que seleccione los n primeros registros                    
-                $aux = $Archivos | select -First 1
-                #elimina un directorio o archivo, en este caso le pasamos la ruta absoluta del .zip a borrar
-                Remove-Item $pathDestino"\"$aux            
-            }     
+        {     
             #Agrego la libreria necesaria para comprimir archivos                           
             Add-Type -Assembly "System.IO.Compression.FileSystem" 
             $instancia = [System.IO.Compression.ZipFile]
@@ -72,13 +61,25 @@ try
             {
                 #Guardo la fecha actual para el nombre del backup
                 $Filename= get-date -Format yyyMMdd
-                $pathDestino = $pathDestino.Insert($pathDestino.Length,"\"+ $filename +".zip")
-                $instancia::CreateFromDirectory($pathOrigen,$pathDestino)
+                $zip = $pathDestino.Insert($pathDestino.Length,"\"+ $filename +".zip")
+                $instancia::CreateFromDirectory($pathOrigen,$zip)
+
+                #Archivos .zip ordenados ascendentemente por fecha de creacion (primero el mas antiguo), se guardan en un array
+                $Archivos = Get-ChildItem $pathDestino -File *.zip| Sort-Object -Property CreationTime
+                #Si hay mas de 3 archivos (.zip)
+                if ($Archivos.Length -gt 3)
+                {
+                    #con -First n indico que seleccione los n primeros registros                    
+                    $aux = $Archivos | select -First 1                
+                    #elimina un directorio o archivo, en este caso le pasamos la ruta absoluta del .zip a borrar
+                    Remove-Item $pathDestino"\"$aux       
+                }
+
                 echo "Backup creado correctamente"
             }
             Catch [System.IO.IOException]
             {
-                echo "El archivo ya existe."
+                echo "Error: El archivo ya existe."
             }
             Catch
             {
