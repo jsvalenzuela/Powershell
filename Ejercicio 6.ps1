1ca6c40fb92f53a606526a624bcea7ffa84affab
Manfred Camacho <manfredcb+gh@gmail.com>
2015-09-08 14:10:10 -0300

    Agregado datos alumnos y ayuda

--- Ejercicio 6.ps1
+++
@@ -1,9 +0,0 @@
-Get-WmiObject -class win32_Processor | Format-Table
-Get-WmiObject -Class Win32_OperatingSystem | Format-Table
-Get-WmiObject -Class Win32_PhysicalMemory|Format-Table Description, ConfiguredClockSpeed, Manufacturer, Name, PartNumber, Capacity
-$hola=Get-WmiObject Win32_NetworkAdapter -filter "AdapterType != NULL" 
-$i=0
-foreach($tarjeta in $hola){
-    $i++
-}
-echo "La cantidad de tarjetas de red que hay en este equipo es/son: $i"
---
+++ Ejercicio_6/ejercicio6.ps1
@@ -0,0 +1,37 @@
+#############################################################################################
+# PROGRAM-ID.  ejercicio6.ps1					                                            #
+# OBJETIVO DEL PROGRAMA: Muestra el modelo de CPU, cantidad de memoria RAM, placas de red y #
+# 						 versión del sistema operativo.                                     #
+# TIPO DE PROGRAMA: .ps1                                                                    #
+# ALUMNOS :                                                                                 #                                                                              #
+#           -Bogado, Sebastian                                                              #
+#           -Camacho, Manfred                                                               #
+#           -Gonzalez, Gustavo                                                              #
+#           -Rey, Juan Cruz                                                                 #
+#           -Valenzuela, Santiago                                                           #
+# Ejemplo Ej.:                                                                              #
+# C:\PS> .\ejercicio6.ps1                                           #
+#############################################################################################
+
+<#
+.SYNOPSIS
+TP1 - PowerShell
+
+.DESCRIPTION
+Muestra el modelo de CPU, cantidad de memoria RAM, placas de red y versión del sistema operativo en formato de tabla.
+
+.EXAMPLE
+    C:\PS> .\ejercicio6.ps1 
+
+	
+#>
+
+Get-WmiObject -class win32_Processor | Format-Table
+Get-WmiObject -Class Win32_OperatingSystem | Format-Table
+Get-WmiObject -Class Win32_PhysicalMemory|Format-Table Description, ConfiguredClockSpeed, Manufacturer, Name, PartNumber, Capacity
+$hola=Get-WmiObject Win32_NetworkAdapter -filter "AdapterType != NULL" 
+$i=0
+foreach($tarjeta in $hola){
+    $i++
+}
+echo "La cantidad de tarjetas de red que hay en este equipo es/son: $i"
